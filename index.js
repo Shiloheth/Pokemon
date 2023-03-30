3770cfc3866cc679957d1099513c832782b492ee
Shiloh <shilohofulue17@gmail.com>
2023-03-30 19:28:43 +0100

    update

--- index.js
+++ index.js
@@ -11,10 +11,55 @@ c.fillRect(0,0,canvas.width,canvas.height)
 const image = new Image()
 image.src = './images/map.png'
 
-const player = new Image()
-player.src = './images/Anoradown.png'
+const playerdown = new Image()
+playerdown.src = './images/Anoradown.png'
 
-console.log(collisions)
+const playerleft = new Image()
+playerleft.src = './images/Anoraleft.png'
+
+const playerup = new Image()
+playerup.src = './images/Anoraup.png'
+
+const playerright = new Image()
+playerright.src = './images/Anoraright.png'
+
+let moving = null
+const map = []
+const Boundaries = []
+
+for(let i = 0;i<collisions.length;i+=41){
+map.push(collisions.slice(i,i+41))
+}
+
+console.log(map)
+
+class Character{
+    constructor({position,image,time}){
+    this.position = position
+    this.image = image.up
+    this.time = time
+    }
+    draw(){
+        c.drawImage(this.image, this.position.x,this.position.y, 62.5, this.image.height, canvas.width/2-this.image.width/4, canvas.height/2-this.image.height/2, this.image.width/4, this.image.height)
+        console.log(this.image.width)
+        if(true){this.time++}
+        if(this.time%15===0){
+        if(this.position.x<249-64&&moving){this.position.x+=64}
+        else this.position.x = 0}
+    }
+}
+
+class Boundary{
+    constructor({position}){
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x,this.position.y,this.width,this.height)
+    }
+}
 
 class Sprite{
     constructor({position,velocity,image}){
@@ -26,10 +71,22 @@ class Sprite{
     }
 }
 
+map.forEach((a,i)=>{
+    a.forEach((b,j)=>{
+      if(b===2945){
+        Boundaries.push(new Boundary({position:{x:j*48,y:i*48}}))
+      }
+    })
+})
+
 const background = new Sprite({image:image,position:{
     x:0,
     y:0
 }})
+
+
+Ash = new Character({image:{down:playerdown,up:playerup,right:playerright,left:playerleft},position:{x:0,y:0},time:1})
+
 
 const keys = {
     ArrowDown:{
@@ -48,28 +105,43 @@ const keys = {
 
 
 
+
 function animation(){
-    window.requestAnimationFrame(animation)
-    background.draw()
-    c.drawImage(player,0,0,player.width/4,player.height,canvas.width/2-player.width/4,canvas.height/2-player.height/2,player.width/4,player.height)
+
+    window.requestAnimationFrame(animation);
+    background.draw();
+    Boundaries.forEach((b) => b.draw());
+    Ash.draw()
 
     if(keys.ArrowDown.isPressed){
-        background.position.y = background.position.y-3
+        background.position.y -=3
+        Boundaries.forEach((a,i)=>a.position.y-=3)
+        Ash.image = playerdown
     }
     if(keys.ArrowUp.isPressed){
         background.position.y = background.position.y+3
+        Boundaries.forEach((a,i)=>a.position.y+=3)
+        Ash.image = playerup
     }
     if(keys.ArrowLeft.isPressed){
         background.position.x = background.position.x+3
+        Boundaries.forEach((a,i)=>a.position.x+=3)
+        Ash.image = playerleft
     }
     if(keys.ArrowRight.isPressed){
         background.position.x = background.position.x-3
+        Boundaries.forEach((a,i)=>a.position.x-=3)
+        Ash.image = playerright
     }
 }
 
 animation()
 
+
+
+
 window.addEventListener('keydown',(e)=>{
+    moving=true
 switch(e.key){
     case 'ArrowDown': keys.ArrowDown.isPressed = true
     break;
@@ -79,10 +151,10 @@ switch(e.key){
     break;
     case 'ArrowRight': keys.ArrowRight.isPressed = true
 } 
-console.log(keys)
 })
 
 window.addEventListener('keyup',(e)=>{
+    moving=false
     switch(e.key){
         case 'ArrowDown': keys.ArrowDown.isPressed = false
         break;
@@ -92,7 +164,6 @@ window.addEventListener('keyup',(e)=>{
         break;
         case 'ArrowRight': keys.ArrowRight.isPressed = false
     } 
-    console.log(keys)
+
     })
 
- 
